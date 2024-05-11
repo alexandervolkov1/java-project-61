28721caeb13f177a61726620999195192c8310fd
a.volkov <a.volkov111555@gmail.com>
2024-05-11 18:15:30 +0300

    stage 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {mainClass.set("hexlet.code.App")}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
