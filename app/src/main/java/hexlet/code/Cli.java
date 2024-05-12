bcd4f9afa988573b2272b89dd7b4de09fcd67b58
a.volkov <a.volkov111555@gmail.com>
2024-05-12 09:47:05 +0300

    add asking name and makefile

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void askName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
+}
